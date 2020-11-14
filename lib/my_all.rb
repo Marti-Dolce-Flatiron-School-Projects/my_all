6daff23a54a1a30848a654509863c6796bdbacc1
Martinique <me@martidolce.com>
2020-11-14 10:44:01 -0500

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
 require 'pry'
 def my_all?(collection)
-	i = 0
-	block_return_values = []
-	while i < collection.length
-		block_return_values << yield(collection[i])
-		i = i + 1
-	end
-
-	if block_return_values.include?(false)
-		false
-	else
-		true
-	end
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i += 1
+  end
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
